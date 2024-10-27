236a692c25abc536dbb2f15581b2ad89ef205ec1
ibnu030303 <ibunu.nurdiyansa28@gmail.com>
2024-10-27 21:23:16 +0700

    Initialize Project

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import express, { Application, Request, Response, NextFunction } from 'express'
+
+const app: Application = express()
+const port: number = 4000
+
+app.use('/health', (req: Request, res: Response, nex: NextFunction) => {
+  res.status(200).send({ status: '200' })
+})
+
+app.listen(port, () => console.log(`Servers Is Listening on port ${port}`))
